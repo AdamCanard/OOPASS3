86f53cfc5ac94d3787d1c08e304bcaf12f91816b
AdamCanard <39779534+AdamCanard@users.noreply.github.com>
2024-12-11 17:31:58 -0700

    added filereading and args reading

--- src/appDomain/WordTracker.java
+++ src/appDomain/WordTracker.java
@@ -1,5 +1,44 @@
 package appDomain;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordTracker {
 
+	public static void main(String[] args) {
+
+		String fileName = args[0];
+		String outputType = args[1];
+		String outputFile = "";
+		if (args.length > 2) {
+			outputFile = args[3];
+		}
+
+		ArrayList<String> lineList = readFile(fileName);
+		for (int i = 0; i < lineList.size(); i++) {
+			System.out.println(lineList.get(i));
+		}
+
+	}
+
+	public static ArrayList<String> readFile(String fileName) {
+		ArrayList<String> fileList = new ArrayList<String>();
+
+		try {
+			File file = new File(fileName);
+			Scanner myReader = new Scanner(file);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				fileList.add(data.trim());
+			}
+
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return fileList;
+	}
 }
